817dbed725298e4fb15558e6a6ece59abd40bc75
jgross888 <56703450+jgross888@users.noreply.github.com>
2019-12-08 15:48:58 -0500

    Add files via upload

---
+++ videodemob.py
@@ -0,0 +1,29 @@
+"""
+Example commands for Tello:
+forward
+backward
+left
+right
+up
+down
+clockwise
+counter_clockwise
+
+Created for Tech Garage
+"""
+
+
+
+from app.Drone import Drone
+
+drone = Drone()
+
+
+
+drone.takeoff()
+drone.sleep(2)
+drone.up(39)
+drone.sleep(2)
+drone.left(122)
+drone.sleep(2)
+drone.land()
